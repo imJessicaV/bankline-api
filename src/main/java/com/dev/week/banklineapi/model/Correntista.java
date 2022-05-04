1d06a7ddfcc260f222e8650d103005d5c3fe98c9
imJessicaV <jessica.svitoriano@gmail.com>
2022-05-03 22:27:51 -0300

    Mapeamento das entidades com JPA

--- src/main/java/com/dev/week/banklineapi/model/Correntista.java
+++ src/main/java/com/dev/week/banklineapi/model/Correntista.java
@@ -1,11 +1,31 @@
 package com.dev.week.banklineapi.model;
 
+import javax.annotation.Generated;
+import javax.persistence.Column;
+import javax.persistence.Embedded;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tab_correntista")
 public class Correntista {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
+	
+	@Column(length = 11)
 	private String cpf;
+	
+	@Column(length = 60)
 	private String nome;
 	
+	@Embedded
 	private Conta conta;
+	
+	//Getters e Setters
 	public Conta getConta() {
 		return conta;
 	}
